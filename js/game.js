487116291e3e406f935f917f4ee1f2a5be32f743
zt64 <31907977+zt64@users.noreply.github.com>
2024-12-02 13:51:12 -0500

    add basic game code

--- js/game.js
+++ js/game.js
@@ -22,6 +22,56 @@ function generateGrid() {
 
     switch (difficulty) {
         case "easy":
+            const cards = availableCards
+                .slice(0, 8)
+                .concat(availableCards.slice(0, 8));
+            shuffle(cards);
+
+            for (let i = 0; i < 16; i++) {
+                const card = document.createElement("div");
+                card.value = cards[i];
+                card.innerHTML = `<img src="img/cards/${cards[i]}" />`;
+                card.classList.add("card");
+                card.addEventListener("click", function () {
+                    if (
+                        card.classList.contains("flipped") ||
+                        card.classList.contains("matched")
+                    ) {
+                        return;
+                    }
+
+                    if (firstCard === null) {
+                        firstCard = card;
+                        firstCard.classList.add("flipped");
+                    } else if (secondCard === null) {
+                        secondCard = card;
+                        secondCard.classList.add("flipped");
+                    }
+
+                    if (firstCard !== null && secondCard !== null) {
+                        if (firstCard.value === secondCard.value) {
+                            firstCard.classList.add("matched");
+                            secondCard.classList.add("matched");
+
+                            firstCard.classList.remove("flipped");
+                            secondCard.classList.remove("flipped");
+
+                            firstCard = null;
+                            secondCard = null;
+                        } else {
+                            setTimeout(() => {
+                                firstCard.classList.remove("flipped");
+                                secondCard.classList.remove("flipped");
+
+                                firstCard = null;
+                                secondCard = null;
+                            }, 1000);
+                        }
+                    }
+                });
+
+                grid.appendChild(card);
+            }
             break;
         case "medium":
             break;
@@ -39,5 +89,8 @@ function shuffle(array) {
         array[j] = temp;
     }
 }
+
+let firstCard = null;
+let secondCard = null;
 
 document.getElementById("start-button").addEventListener("click", generateGrid);
