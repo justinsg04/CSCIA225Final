8b4c21fd52c764d427296b2f20049a18facdb1ba
justinsg04 <gloverj820@gmail.com>
2024-12-03 00:12:53 -0500

    popup try

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 document.getElementById("play-button").addEventListener("click", function () {
     window.location.href = "game.html";
 });
+
+// how to play popup
+const instructionsButton = document.getElementById('instructions-button');
+const popup = document.getElementById('how-to-play-popup');
+const closeButton = document.querySelector('.popupCloseButton');
+
+instructionsButton.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+popup.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+});
+
