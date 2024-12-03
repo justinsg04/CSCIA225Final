4e56c82dc0521e0a3b03ba8b9dc403f6b8a7f332
justinsg04 <gloverj820@gmail.com>
2024-12-03 16:22:39 -0500

    .

--- js/index.js
+++ js/index.js
@@ -4,10 +4,15 @@ document.getElementById("play-button").addEventListener("click", function () {
 
 // how to play popup
 const instructionsButton = document.getElementById('instructions-button');
+const howtoButton = document.getElementById('howto')
 const popup = document.getElementById('how-to-play-popup');
 const closeButton = document.querySelector('.popupCloseButton');
 
 instructionsButton.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+
+howtoButton.addEventListener('click', () => {
     popup.style.display = 'block';
 });
 
