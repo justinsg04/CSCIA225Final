b572e3285e9fb5d23eab3fe6da59566d75daac40
justinsg04 <gloverj820@gmail.com>
2024-12-07 01:42:21 -0500

    pop-ups

--- js/index.js
+++ js/index.js
@@ -9,24 +9,44 @@ document.getElementById("leaderboard-btn").addEventListener("click", function ()
 // how to play popup
 const instructionsButton = document.getElementById('instructions-button');
 const howtoButton = document.getElementById('howto')
-const popup = document.getElementById('how-to-play-popup');
+const popup1 = document.getElementById('how-to-play-popup');
 const closeButton = document.querySelector('.popupCloseButton');
 
+const aboutUsButton = document.getElementById('aboutus');
+const popup2 = document.getElementById('aboutus_popup');
+const closeButton2 = document.querySelector('.close-about-us');
+//--------------
 instructionsButton.addEventListener('click', () => {
-    popup.style.display = 'block';
+    popup1.style.display = 'block';
 });
 
 howtoButton.addEventListener('click', () => {
-    popup.style.display = 'block';
+    popup1.style.display = 'block';
 });
 
 closeButton.addEventListener('click', () => {
-    popup.style.display = 'none';
+    popup1.style.display = 'none';
 });
 
-popup.addEventListener('click', (event) => {
-    if (event.target === popup) {
-        popup.style.display = 'none';
+popup1.addEventListener('click', (event) => {
+    if (event.target === popup1) {
+        popup1.style.display = 'none';
     }
 });
+
+//------------
+aboutUsButton.addEventListener('click', () => {
+    popup2.style.display = 'block';
+});
+
+closeButton2.addEventListener('click', () => {
+    popup2.style.display = 'none';
+});
+
+popup2.addEventListener('click', (event) => {
+    if (event.target === popup2) {
+        popup2.style.display = 'none';
+    }
+});
+
 
