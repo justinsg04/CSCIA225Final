700c10832879cb7e35652ca24e8fb08c2ae1b337
zt64 <31907977+zt64@users.noreply.github.com>
2024-12-10 15:34:58 -0500

    fix errors

---
+++ js/home.js
@@ -0,0 +1,9 @@
+document.getElementById("play-button").addEventListener("click", function () {
+    window.location.href = "game.html";
+});
+
+document
+    .getElementById("leaderboard-btn")
+    .addEventListener("click", function () {
+        window.location.href = "leaderboard.html";
+    });
--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
-document.getElementById("play-button").addEventListener("click", function () {
-    window.location.href = "game.html";
-});
 
-document.getElementById("leaderboard-btn").addEventListener("click", function () {
-    window.location.href = "leaderboard.html";
-});
 
 // how to play popup
-const instructionsButton = document.getElementById('instructions-button');
 const howtoButton = document.getElementById('howto')
 const popup1 = document.getElementById('how-to-play-popup');
 const closeButton = document.querySelector('.popupCloseButton');
@@ -16,9 +9,6 @@ const aboutUsButton = document.getElementById('aboutus');
 const popup2 = document.getElementById('aboutus_popup');
 const closeButton2 = document.querySelector('.close-about-us');
 //--------------
-instructionsButton.addEventListener('click', () => {
-    popup1.style.display = 'block';
-});
 
 howtoButton.addEventListener('click', () => {
     popup1.style.display = 'block';
@@ -48,5 +38,3 @@ popup2.addEventListener('click', (event) => {
         popup2.style.display = 'none';
     }
 });
-
-
